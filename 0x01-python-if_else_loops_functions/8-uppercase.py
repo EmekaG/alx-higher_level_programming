65d6d73cd860728b19d532fe7da1d1098547ef94
EmekaG <nellychukwemeka@gmail.com>
2022-11-30 11:24:20 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def alphupper(alph):
-    if ord(alph) >= 97 and ord(alph) <= 122:
-        return (ord(alph) - 32)
-    else:
-        return ord(alph)
-
-def uppercase(string):
-    string_new = ""
-    for alph in string:
-        string_new += "%c" % alphupper(alph)
-        print("{:s}".format(string_new))
+def uppercase(str):
+    for s in str:
+        if ord(s) >= 97 and ord(s) <= 122:
+            s = chr(ord(s) - 32)
+        print("{}".format(s), end="")
+    print()
