f1eccbadd16a12099deb219d666f04c73c553e08
EmekaG <nellychukwemeka@gmail.com>
2022-11-30 11:04:50 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for number in range(00, 100):
-    print("{:02}".format(number), end='\n' if number == 99 else ", ")
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
