df45afd053f307fb6c87d99cc89c83fe22e5876c
EmekaG <nellychukwemeka@gmail.com>
2022-12-05 08:41:18 -0800

    4-new_in_list.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_str = ''
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            new_str += i
-        return (new_str)
+    copy = [n for n in my_string if n != 'c' and n != 'C']
+    return ("".join(copy))
