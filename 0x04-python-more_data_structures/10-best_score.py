a4502e78e67f29fce2376896ec1eabcbee8cda97
EmekaG <nellychukwemeka@gmail.com>
2022-12-07 18:07:06 -0800

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, ref) or len(a_dictionary) == 0:
+        return None
+    mot = list(a_dictionary.keys())[0]
+    cov = a_dictionary[mot]
+    for r, g in a_dictionary.items():
+        if g > cov:
+            cov = g
+            mot = r
+    return (mot)
